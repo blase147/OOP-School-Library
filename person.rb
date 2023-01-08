10f87ef37eb41bb01827693b312cfc921f9a9e6a
blase147 <solarmails2@gmail.com>
2023-01-08 17:35:42 +0100

    Modified person.rb, and nameable(corrector.rb)

--- person.rb
+++ person.rb
@@ -1,14 +1,22 @@
+require_relative './corrector'
 class Person
   attr_accessor :name, :age
   attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
-    @id = Random.ran(1..1000)
+    @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @corrector = Corrector.new
   end
-  private :of_age?
+
+  def validate_name
+    @name = @corrector.correct_name(name)
+  end
+
+  private
+
   def of_age?
     @age >= 18
   end
@@ -17,3 +25,8 @@ class Person
     of_age? || parent_permission == true
   end
 end
+
+person = Person.new(100, 'chukwumaijemnuwaa')
+person.validate_name
+
+puts person.name
